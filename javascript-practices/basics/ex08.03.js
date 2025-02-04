5bbc67ecafd63e475e0c6e13a149f4eb28a09be4
h7eona <jessie117@naver.com>
2025-02-04 16:41:37 +0900

    javascript #2

--- javascript-practices/basics/ex08.03.js
+++ javascript-practices/basics/ex08.03.js
@@ -4,7 +4,37 @@
     ex) List 함수 지원하기
 
 */
+Array.prototype.remove = function(index) {
+    this.splice(index, 1);
+}
 
+Array.prototype.insert = function(index, value){
+    if(value instanceof Array){
+        // for(var i = 0; i < value.length; i++){
+        //     this.splice(index+i, 0, value[i]);
+        // }
+
+        // 오류!
+        // 콜백 함수에서의 this는 어휘상의 this와 일치하지 않는다.
+        // value.forEach(function(e){
+        //     //console.log(e, this);
+        // });
+
+        // 해결방법 1: 
+        // var _this = this;
+        // value.forEach(function(e){
+        //     _this.splice(index++, 0, e);
+        // });
+
+        // 해결방법 2:
+        value.forEach(function(e){
+            this.splice(index++, 0, e);
+        }.bind(this));
+
+    } else{
+        this.splice(index, 0, value);
+    }    
+}
 
 
 
